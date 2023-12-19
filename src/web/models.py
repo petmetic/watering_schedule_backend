0c9531728354b67bcabec6efaba786a2618c4648
Meta <petmetic@gmail.com>
2023-12-19 11:25:06 +0100

    added plant model

--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+LOCATION_CHOICES = (
+    ('LRBT', 'living room black table & around'),
+    ('LRH', 'living room hanging'),
+    ('LRWT', 'living room white tables'),
+    ('BS', 'bookshelves'),
+    ('OWS', 'outside window sill'),
+    ('IWS', 'inside window sill - living room'),
+    ('B1', 'bedroom 1'),
+    ('B2', 'bedroom 2'),
+    ('SC', 'special care')
+)
+
+
+class Plant(models.Model):
+    name = models.CharField(max_length=200, default="", blank=True)
+    location = models.CharField(choices=LOCATION_CHOICES, default='LRWT')
+    frequency = models.IntegerField(default=1, blank=True, null=True)
+    instructions = models.TextField(default="", blank=True)
+    start = models.DateTimeField(blank=True, null=True)
+    end = models.DateTimeField(blank=True, null=True)
+
+    added = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
