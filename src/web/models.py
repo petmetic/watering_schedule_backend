369ee72dd434318ae43aea13ad7c6f5211460e26
Meta <petmetic@gmail.com>
2024-02-01 13:09:40 +0100

    flake8 lint fixes2

--- src/web/models.py
+++ src/web/models.py
@@ -3,41 +3,40 @@ from django.db import models
 
 class Plant(models.Model):
     LOCATION_CHOICES = (
-        ('living_room_black', 'living room black table & around'),
-        ('living_room_hanging', 'living room hanging'),
-        ('living_room_white', 'living room white tables'),
-        ('bookshelves', 'bookshelves'),
-        ('window_outside', 'outside window sill'),
-        ('window_inside', 'inside window sill - living room'),
-        ('bedroom1', 'bedroom 1'),
-        ('bedroom2', 'bedroom 2'),
-        ('special_care', 'special care')
+        ("living_room_black", "living room black table & around"),
+        ("living_room_hanging", "living room hanging"),
+        ("living_room_white", "living room white tables"),
+        ("bookshelves", "bookshelves"),
+        ("window_outside", "outside window sill"),
+        ("window_inside", "inside window sill - living room"),
+        ("bedroom1", "bedroom 1"),
+        ("bedroom2", "bedroom 2"),
+        ("special_care", "special care"),
     )
 
     WATER_VOLUME = (
-        ('100_ml', '100 ml'),
-        ('200_ml', '200 ml'),
-        ('300_ml', '300 ml'),
-        ('400_ml', '400 ml'),
-        ('500_ml', '500 ml')
+        ("100_ml", "100 ml"),
+        ("200_ml", "200 ml"),
+        ("300_ml", "300 ml"),
+        ("400_ml", "400 ml"),
+        ("500_ml", "500 ml"),
     )
-    STATUS = (
-        ('watered', 'watered'),
-        ('needs_watering', 'needs watering')
-    )
+    STATUS = (("watered", "watered"), ("needs_watering", "needs watering"))
 
     name = models.CharField(max_length=200, default="")
-    location = models.CharField(max_length=100, choices=LOCATION_CHOICES, default='living_room_white')
+    location = models.CharField(
+        max_length=100, choices=LOCATION_CHOICES, default="living_room_white"
+    )
     frequency = models.IntegerField(default=1, blank=True, null=True)
-    volume = models.CharField(max_length=100, choices=WATER_VOLUME, default='100_ml')
+    volume = models.CharField(max_length=100, choices=WATER_VOLUME, default="100_ml")
     instructions = models.TextField(default="", blank=True)
     start = models.DateTimeField(blank=True, null=True)
     end = models.DateTimeField(blank=True, null=True)
-    status = models.CharField(max_length=100, choices=STATUS, default='needs_watering')
-    photo = models.ImageField(upload_to='uploads/', default="")
+    status = models.CharField(max_length=100, choices=STATUS, default="needs_watering")
+    photo = models.ImageField(upload_to="uploads/", default="")
 
     added = models.DateTimeField(auto_now_add=True)
     changed = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
