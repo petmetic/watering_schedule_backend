6a87ab7297c0d415e41a4d56cd75ccd86d4ffd31
Meta <petmetic@gmail.com>
2024-03-15 11:27:44 +0100

    added cors to settings.py

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.admindocs",
     "rest_framework",
+    "corsheaders",
     "web",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
