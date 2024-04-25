22bedb9fb4c89eeff12d96980e296bad065c94d2
Meta <petmetic@gmail.com>
2024-04-25 10:08:18 +0200

    url refactor

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "sl-SI"
+LANGUAGE_CODE = "en-En"
 
-TIME_ZONE = "Europe/Berlin"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
