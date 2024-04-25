acd469a0c076ea5135680fab8e6a74716042f756
Meta <petmetic@gmail.com>
2024-04-25 13:18:54 +0200

    git merge from main branch fix

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -19,13 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-
-from web import views
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"plants", views.PlantViewSet)
 
 
 # Wire up our API using automatic URL routing.
@@ -34,11 +27,9 @@ router.register(r"plants", views.PlantViewSet)
 urlpatterns = [
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", include(router.urls)),
+    path("", include("web.urls")),
 ]
 
-urlpatterns += router.urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/web/urls.py
+++ src/web/urls.py
@@ -1,5 +1,6 @@
-from rest_framework import routers
+
 from django.urls import include, path
+from rest_framework import routers
 
 from web import views
 
